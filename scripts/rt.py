bb722869c0819dad485ebae4dd38cc4d68c8ef4c
Tomáš Rus <rustomas7@gmail.com>
2019-03-07 15:16:06 +0100

    Additional changes
    Added message prints to all runs.
    Can run tests over entire directories now.

--- scripts/rt.py
+++ scripts/rt.py
@@ -3,10 +3,11 @@
 import subprocess
 import argparse
 import tempfile
-import os.path
+import os
 
 
 def run_cmd(cmd, timeout):
+    print("Running: ", cmd)
     return subprocess.run(cmd, capture_output=True,
                           timeout=timeout, text=True)
 
@@ -17,53 +18,45 @@ def compile(clang, input, output, timeout, options=None):
     if options is not None:
         cmd.extend(options)
     cmd.extend([input, "-o", output])
+    p = run_cmd(cmd, timeout)
+    assert p.returncode == 0, \
+        "Clang failure."
+    assert len(p.stderr) == 0, \
+        "errors or warnings during compilation: " + p.stderr
 
-    return run_cmd(cmd, timeout)
+    return p
 
 
 def decompile(rellic, input, output, timeout):
     cmd = []
     cmd.append(rellic)
     cmd.extend(["--input", input, "--output", output])
+    p = run_cmd(cmd, timeout)
+    assert p.returncode == 0, \
+        "rellic-decomp failure."
+    assert len(p.stderr) == 0, \
+        "errors or warnings during decompilation: " + p.stderr
 
-    return run_cmd(cmd, timeout)
+    return p
 
 
 def roundtrip(rellic, filename, clang, timeout):
     with tempfile.TemporaryDirectory() as tempdir:
-        # test compilation to executable
+        print ("Testing: ", filename)
         out1 = os.path.join(tempdir, "out1")
-        p = compile(clang, filename, out1, timeout)
-        assert p.returncode == 0, \
-            "Clang failure."
-        assert len(p.stderr) == 0, \
-            "errors or warnings during compilation: " + p.stderr
+        compile(clang, filename, out1, timeout)
 
         # capture binary run outputs
         cp1 = run_cmd(out1, timeout)
 
-        # test compilation to bitcode
         rt_bc = os.path.join(tempdir, "rt.bc")
-        p = compile(clang, filename, rt_bc, timeout, ["-c", "-emit-llvm"])
-        assert p.returncode == 0, \
-            "Clang failure."
-        assert len(p.stderr) == 0, \
-            "errors or warnings during compilation: " + p.stderr
+        compile(clang, filename, rt_bc, timeout, ["-c", "-emit-llvm"])
 
-        # test decompilation from bitcode to C code
         rt_c = os.path.join(tempdir, "rt.c")
-        p = decompile(rellic, rt_bc, rt_c, timeout)
-        assert p.returncode == 0, \
-            "rellic-decomp failure."
-        assert len(p.stderr) == 0, \
-            "errors or warnings during decompilation: " + p.stderr
+        decompile(rellic, rt_bc, rt_c, timeout)
 
-        # test compilation to executable
         out2 = os.path.join(tempdir, "out2")
-        p = compile(clang, rt_c, out2, timeout)
-        assert p.returncode == 0, "Clang failure."
-#       assert len(p.stderr) == 0, \
-#           "errors or warnings during compilation: " + p.stderr
+        compile(clang, rt_c, out2, timeout, ["-Wno-everything"])
 
         # capture outputs of binary after roundtrip
         cp2 = run_cmd(out2, timeout)
@@ -74,13 +67,14 @@ def roundtrip(rellic, filename, clang, timeout):
         "Different stdout."
     assert cp1.returncode == cp2.returncode, \
         "Different return code."
+    print("-------------------------------")
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("rellic", help="path to rellic-decomp")
-    parser.add_argument("filename",
-                        help="path to source code test file")
+    parser.add_argument("test_path",
+                        help="path to source code test file(s)")
     parser.add_argument("clang", help="path to clang")
     parser.add_argument(
         "-t",
@@ -88,7 +82,12 @@ def main():
         help="set timeout in seconds",
         type=int)
     args = parser.parse_args()
-    roundtrip(args.rellic, args.filename, args.clang, args.timeout)
+    if os.path.isdir(args.test_path):
+        with os.scandir(args.test_path) as it:
+            for item in it:
+                roundtrip(args.rellic, item.path, args.clang, args.timeout)
+    else:
+        roundtrip(args.rellic, args.test_path, args.clang, args.timeout)
 
 
 if __name__ == "__main__":

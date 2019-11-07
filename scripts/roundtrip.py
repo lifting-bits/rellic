acfee0cba30184c1e3b9fc1d00be361fb3b158f1
Marek Surovič <marek.surovic@gmail.com>
2019-11-07 13:37:38 +0100

    Replaces `assert` with `unittest.TestCase.assert*` methods
    This should prevent the interpreter optimizing out assertions

--- scripts/roundtrip.py
+++ scripts/roundtrip.py
@@ -7,6 +7,7 @@ import tempfile
 import os
 import sys
 
+
 class RunError(Exception):
     def __init__(self, msg):
         self.msg = msg
@@ -33,62 +34,63 @@ def run_cmd(cmd, timeout):
     return p
 
 
-def compile(clang, input, output, timeout, options=None):
+def compile(self, clang, input, output, timeout, options=None):
     cmd = []
     cmd.append(clang)
     if options is not None:
         cmd.extend(options)
     cmd.extend([input, "-o", output])
     p = run_cmd(cmd, timeout)
-    assert p.returncode == 0, \
-        "clang failure"
-    assert len(p.stderr) == 0, \
-        "errors or warnings during compilation: " + p.stderr
+
+    self.assertEqual(p.returncode, 0, "clang failure")
+    self.assertEqual(len(p.stderr), 0,
+                     "errors or warnings during compilation: %s" % p.stderr)
+
     return p
 
 
-def decompile(rellic, input, output, timeout):
+def decompile(self, rellic, input, output, timeout):
     cmd = []
     cmd.append(rellic)
     cmd.extend(["--input", input, "--output", output])
     p = run_cmd(cmd, timeout)
-    assert p.returncode == 0, \
-        "rellic-decomp failure: " + p.stderr
-    assert len(p.stderr) == 0, \
-        "errors or warnings during decompilation: " + p.stderr
+
+    self.assertEqual(p.returncode, 0, "rellic-decomp failure: %s" % p.stderr)
+    self.assertEqual(len(p.stderr), 0,
+                     "errors or warnings during decompilation: %s" % p.stderr)
+
     return p
 
 
-def roundtrip(rellic, filename, clang, timeout):
+def roundtrip(self, rellic, filename, clang, timeout):
     with tempfile.TemporaryDirectory() as tempdir:
         out1 = os.path.join(tempdir, "out1")
-        compile(clang, filename, out1, timeout)
+        compile(self, clang, filename, out1, timeout)
 
         # capture binary run outputs
         cp1 = run_cmd([out1], timeout)
 
         rt_bc = os.path.join(tempdir, "rt.bc")
-        compile(clang, filename, rt_bc, timeout, ["-c", "-emit-llvm"])
+        compile(self, clang, filename, rt_bc, timeout, ["-c", "-emit-llvm"])
 
         rt_c = os.path.join(tempdir, "rt.c")
-        decompile(rellic, rt_bc, rt_c, timeout)
+        decompile(self, rellic, rt_bc, rt_c, timeout)
 
         out2 = os.path.join(tempdir, "out2")
-        compile(clang, rt_c, out2, timeout, ["-Wno-everything"])
+        compile(self, clang, rt_c, out2, timeout, ["-Wno-everything"])
 
         # capture outputs of binary after roundtrip
         cp2 = run_cmd([out2], timeout)
 
-        assert cp1.stderr == cp2.stderr, \
-            "Different stderr"
-        assert cp1.stdout == cp2.stdout, \
-            "Different stdout"
-        assert cp1.returncode == cp2.returncode, \
-            "Different return code"
+        self.assertEqual(cp1.stderr, cp2.stderr, "Different stderr")
+        self.assertEqual(cp1.stdout, cp2.stdout, "Different stdout")
+        self.assertEqual(cp1.returncode, cp2.returncode,
+                         "Different return code")
 
 
 class TestRoundtrip(unittest.TestCase):
     pass
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -101,18 +103,18 @@ if __name__ == "__main__":
         "--timeout",
         help="set timeout in seconds",
         type=int)
-    
+
     args = parser.parse_args()
 
     def test_generator(path):
         def test(self):
-            roundtrip(args.rellic, path, args.clang, args.timeout)
+            roundtrip(self, args.rellic, path, args.clang, args.timeout)
         return test
-    
+
     with os.scandir(args.tests) as it:
         for item in it:
             test_name = 'test_%s' % os.path.splitext(item.name)[0]
             test = test_generator(item.path)
             setattr(TestRoundtrip, test_name, test)
-    
+
     unittest.main(argv=[sys.argv[0]])

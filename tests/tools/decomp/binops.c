75366e7cdd9cbf18a13a4bac92e2e2377cce2f01
meme <meme@users.noreply.github.com>
2019-11-22 17:32:34 -0500

    Add tests for BinOp changes

---
+++ tests/tools/decomp/binops.c
@@ -0,0 +1,21 @@
+__attribute__ ((used))
+unsigned int target(unsigned int n) {
+  unsigned int mod = n % 4;
+  unsigned int result = 0;
+
+  if (mod == 0) {
+    result = (n | 0xbaaad0bf) * (2 ^ n);
+  } else if (mod == 1) {
+    result = (n & 0xbaaad0bf) * (3 + n);
+  } else if (mod == 2) {
+    result = (n ^ 0xbaaad0bf) * (4 | n);
+  } else {
+    result = (n + 0xbaaad0bf) * (5 & n);
+  }
+
+  return result;
+}
+
+int main(void) {
+    return 0;
+}

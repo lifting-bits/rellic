8c8d5f40a7562c7dd75805d0a81c88e1a0260bb7
Marek Surovič <marek.surovic@gmail.com>
2020-06-23 18:07:33 +0200

    Fixes compatibility with clang-9

--- rellic/AST/Compat/Expr.cpp
+++ rellic/AST/Compat/Expr.cpp
@@ -62,4 +62,17 @@ clang::Expr *CreateCallExpr(clang::ASTContext &ctx, clang::Expr *func,
 #endif
 }
 
+clang::Expr *CreateMemberExpr(clang::ASTContext &ctx, clang::Expr *base,
+                              clang::ValueDecl *member, clang::QualType type,
+                              bool is_arrow) {
+#if LLVM_VERSION_NUMBER >= LLVM_VERSION(9, 0)
+  return clang::MemberExpr::CreateImplicit(
+      ctx, base, is_arrow, member, type, clang::VK_RValue, clang::OK_Ordinary);
+#else
+  return new (ctx) clang::MemberExpr(base, is_arrow, clang::SourceLocation(),
+                                     member, clang::SourceLocation(), type,
+                                     clang::VK_RValue, clang::OK_Ordinary);
+#endif
+}
+
 }  // namespace rellic

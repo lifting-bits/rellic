7739704843ab8236cab10e22df3b2d2f0121885a
Marek Surovič <marek.surovic@gmail.com>
2021-03-18 18:50:46 +0100

    Adds missing file.
    * I really hope this one passes. I'm getting sick of the GH spam.

---
+++ unittests/UnitTest.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021-present, Trail of Bits, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed in accordance with the terms specified in
+ * the LICENSE file found in the root directory of this source tree.
+ */
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>

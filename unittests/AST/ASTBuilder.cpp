42aa3b42059283dffc03b03749957e560845d7f8
Marek Surovič <marek.surovic@gmail.com>
2021-03-22 20:23:48 +0100

    Factors out clang::ASTUnit creation code

--- unittests/AST/ASTBuilder.cpp
+++ unittests/AST/ASTBuilder.cpp
@@ -11,16 +11,24 @@
 #include <clang/Tooling/Tooling.h>
 #include <doctest/doctest.h>
 
-TEST_SUITE("ASTBuilder::CreateIntLit") {
+namespace {
+static std::unique_ptr<clang::ASTUnit> GetEmptyASTUnit() {
+  auto unit{clang::tooling::buildASTFromCode("", "out.c")};
+  REQUIRE(unit != nullptr);
+  return unit;
+}
+}  // namespace
+
+// TODO(surovic): Add test cases for signed `llvm::APInt` and group
+// via `SCENARIO` rather than `TEST_SUITE`. This needs better
+// support for value-parametrized tests from doctest. Should be in
+// version 2.5.0
+TEST_SUITE("ASTBuilder::CreateIntLit") {   
   SCENARIO("Create clang::IntegerLiteral for 1 bit integer value") {
     GIVEN("Empty clang::ASTContext") {
-      auto unit{clang::tooling::buildASTFromCode("", "out.c")};
-
-      REQUIRE(unit != nullptr);
-
+      auto unit{GetEmptyASTUnit()};
       auto &ctx{unit->getASTContext()};
       rellic::ASTBuilder ast(ctx);
-
       GIVEN("1 bit unsigned llvm::APInt") {
         llvm::APInt api(1U, 0U, /*isSigned=*/false);
         auto lit{ast.CreateIntLit(api)};
@@ -35,13 +43,9 @@ TEST_SUITE("ASTBuilder::CreateIntLit") {
 
   SCENARIO("Create clang::IntegerLiteral for 8 bit integer value") {
     GIVEN("Empty clang::ASTContext") {
-      auto unit{clang::tooling::buildASTFromCode("", "out.c")};
-
-      REQUIRE(unit != nullptr);
-
+      auto unit{GetEmptyASTUnit()};
       auto &ctx{unit->getASTContext()};
       rellic::ASTBuilder ast(ctx);
-
       GIVEN("8 bit unsigned llvm::APInt") {
         llvm::APInt api(8U, 42U, /*isSigned=*/false);
         auto lit{ast.CreateIntLit(api)};
@@ -57,19 +61,16 @@ TEST_SUITE("ASTBuilder::CreateIntLit") {
 
   SCENARIO("Create clang::IntegerLiteral for 16 bit integer value") {
     GIVEN("Empty clang::ASTContext") {
-      auto unit{clang::tooling::buildASTFromCode("", "out.c")};
-
-      REQUIRE(unit != nullptr);
-
+      auto unit{GetEmptyASTUnit()};
       auto &ctx{unit->getASTContext()};
       rellic::ASTBuilder ast(ctx);
-
       GIVEN("16 bits wide llvm::APInt") {
         llvm::APInt api(16U, 42U, /*isSigned=*/false);
         auto lit{ast.CreateIntLit(api)};
         THEN("return a `unsigned short` typed integer literal") {
           REQUIRE(lit != nullptr);
-          CHECK(clang::isa<clang::IntegerLiteral>(lit));
+          CHECK(clang::isa<clang::CStyleCastExpr>(lit));
+          CHECK(clang::isa<clang::IntegerLiteral>(lit->IgnoreCasts()));
           CHECK(lit->getType() == ctx.UnsignedShortTy);
         }
       }
@@ -78,13 +79,9 @@ TEST_SUITE("ASTBuilder::CreateIntLit") {
 
   SCENARIO("Create clang::IntegerLiteral for 32 bit integer value") {
     GIVEN("Empty clang::ASTContext") {
-      auto unit{clang::tooling::buildASTFromCode("", "out.c")};
-
-      REQUIRE(unit != nullptr);
-
+      auto unit{GetEmptyASTUnit()};
       auto &ctx{unit->getASTContext()};
       rellic::ASTBuilder ast(ctx);
-
       GIVEN("32 bits wide llvm::APInt") {
         llvm::APInt api(32U, 42U, /*isSigned=*/false);
         auto lit{ast.CreateIntLit(api)};
@@ -99,13 +96,9 @@ TEST_SUITE("ASTBuilder::CreateIntLit") {
 
   SCENARIO("Create clang::IntegerLiteral for 64 bit integer value") {
     GIVEN("Empty clang::ASTContext") {
-      auto unit{clang::tooling::buildASTFromCode("", "out.c")};
-
-      REQUIRE(unit != nullptr);
-
+      auto unit{GetEmptyASTUnit()};
       auto &ctx{unit->getASTContext()};
       rellic::ASTBuilder ast(ctx);
-
       GIVEN("64 bits wide llvm::APInt") {
         llvm::APInt api(64U, 42U, /*isSigned=*/false);
         auto lit{ast.CreateIntLit(api)};
@@ -122,13 +115,9 @@ TEST_SUITE("ASTBuilder::CreateIntLit") {
 TEST_SUITE("ASTBuilder::CreateCharLit") {
   SCENARIO("Create clang::CharacterLiteral for 8 bit integer value") {
     GIVEN("Empty clang::ASTContext") {
-      auto unit{clang::tooling::buildASTFromCode("", "out.c")};
-
-      REQUIRE(unit != nullptr);
-
+      auto unit{GetEmptyASTUnit()};
       auto &ctx{unit->getASTContext()};
       rellic::ASTBuilder ast(ctx);
-
       GIVEN("8 bit unsigned llvm::APInt") {
         llvm::APInt api(8U, 'x', /*isSigned=*/false);
         auto lit{ast.CreateCharLit(api)};
@@ -145,20 +134,18 @@ TEST_SUITE("ASTBuilder::CreateCharLit") {
 TEST_SUITE("ASTBuilder::CreateStrLit") {
   SCENARIO("Create clang::StringLiteral from std::string") {
     GIVEN("Empty clang::ASTContext") {
-      auto unit{clang::tooling::buildASTFromCode("", "out.c")};
-
-      REQUIRE(unit != nullptr);
-
+      auto unit{GetEmptyASTUnit()};
       auto &ctx{unit->getASTContext()};
       rellic::ASTBuilder ast(ctx);
-
       GIVEN("std::string object by value") {
-        // auto lit{ast.CreateStrLit("a string")};
-        // THEN("return a `int` typed character literal") {
-        //   REQUIRE(lit != nullptr);
-        //   CHECK(clang::isa<clang::CharacterLiteral>(lit));
-        //   CHECK(lit->getType() == ctx.IntTy);
-        // }
+        std::string str("a string");
+        auto lit{ast.CreateStrLit(str)};
+        THEN("return a `char[]` typed character literal") {
+          REQUIRE(lit != nullptr);
+          CHECK(clang::isa<clang::StringLiteral>(lit));
+          CHECK(lit->getType() ==
+                ctx.getStringLiteralArrayType(ctx.CharTy, str.size()));
+        }
       }
     }
   }
@@ -167,13 +154,9 @@ TEST_SUITE("ASTBuilder::CreateStrLit") {
 TEST_SUITE("ASTBuilder::CreateFPLit") {
   SCENARIO("Create clang::FloatingLiteral for 32 bit IEEE 754 value") {
     GIVEN("Empty clang::ASTContext") {
-      auto unit{clang::tooling::buildASTFromCode("", "out.c")};
-
-      REQUIRE(unit != nullptr);
-
+      auto unit{GetEmptyASTUnit()};
       auto &ctx{unit->getASTContext()};
       rellic::ASTBuilder ast(ctx);
-
       GIVEN("`float` initialized llvm::APFloat") {
         auto lit{ast.CreateFPLit(llvm::APFloat(float(3.14)))};
         THEN("return a `float` typed floating point literal") {
@@ -187,13 +170,9 @@ TEST_SUITE("ASTBuilder::CreateFPLit") {
 
   SCENARIO("Create clang::FloatingLiteral for 64 bit IEEE 754 value") {
     GIVEN("Empty clang::ASTContext") {
-      auto unit{clang::tooling::buildASTFromCode("", "out.c")};
-
-      REQUIRE(unit != nullptr);
-
+      auto unit{GetEmptyASTUnit()};
       auto &ctx{unit->getASTContext()};
       rellic::ASTBuilder ast(ctx);
-
       GIVEN("`double` initialized llvm::APFloat") {
         auto lit{ast.CreateFPLit(llvm::APFloat(double(3.14)))};
         THEN("Return a `double` typed floating point literal") {

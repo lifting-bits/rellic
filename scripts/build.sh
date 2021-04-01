62c13de9dc3a9de6bd0aeb909d420651254a0dca
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-04-01 17:29:37 -0400

    Remove old cxx-common and just use vcpkg

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,24 +16,19 @@
 # General directory structure:
 #   /path/to/home/rellic
 #   /path/to/home/rellic-build
+#   /path/to/home/lifting-bits-downloads
 
 SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SRC_DIR=$( cd "$( dirname "${SCRIPTS_DIR}" )" && pwd )
+DOWNLOAD_DIR="$( cd "$( dirname "${SRC_DIR}" )" && pwd )/lifting-bits-downloads"
 CURR_DIR=$( pwd )
 BUILD_DIR="${CURR_DIR}/rellic-build"
 INSTALL_DIR=/usr/local
-LLVM_VERSION=llvm900
+LLVM_VERSION=llvm-11
 OS_VERSION=unknown
 ARCH_VERSION=unknown
 BUILD_FLAGS=
-USE_HOST_COMPILER=0
-LIBRARIES="${BUILD_DIR}/libraries"
 CXX_COMMON_VERSION="v0.1.1"
-
-Z3_ARCHIVE=z3-
-Z3_VERSION=4.7.1
-Z3_ARCH_VERSION=x64
-Z3_OS_VERSION=ubuntu-16.04
 
 # There are pre-build versions of various libraries for specific
 # Ubuntu releases.
@@ -43,37 +38,37 @@ function GetUbuntuOSVersion
   source /etc/lsb-release
 
   case "${DISTRIB_CODENAME}" in
+    groovy)
+      echo "[!] Ubuntu 20.10 is not supported; using libraries for Ubuntu 20.04 instead"
+      OS_VERSION=ubuntu-20.04
+      return 0
+    ;;
     focal)
-      OS_VERSION=ubuntu20.04
+      OS_VERSION=ubuntu-20.04
+      return 0
+    ;;
+    eoam)
+      echo "[!] Ubuntu 19.10 is not supported; using libraries for Ubuntu 18.04 instead"
+      OS_VERSION=ubuntu-18.04
       return 0
     ;;
     disco)
-      OS_VERSION=ubuntu18.04
-      return 0
-    ;;
-    dingo)
-      OS_VERSION=ubuntu19.04
+      echo "[!] Ubuntu 19.04 is not supported; using libraries for Ubuntu 18.04 instead"
+      OS_VERSION=ubuntu-18.04
       return 0
     ;;
     cosmic)
-      OS_VERSION=ubuntu18.10
+      echo "[!] Ubuntu 18.10 is not supported; using libraries for Ubuntu 18.04 instead"
+      OS_VERSION=ubuntu-18.04
       return 0
     ;;
     bionic)
-      OS_VERSION=ubuntu18.04
-      return 0
-    ;;
-    xenial)
-      OS_VERSION=ubuntu16.04
-      return 0
-    ;;
-    trusty)
-      USE_HOST_COMPILER=1
-      OS_VERSION=ubuntu14.04
+      OS_VERSION=ubuntu-18.04
       return 0
     ;;
     *)
-      echo "[x] Ubuntu ${DISTRIB_CODENAME} is not supported."
+      echo "[x] Ubuntu ${DISTRIB_CODENAME} is not supported. Only focal (20.04) and bionic (18.04) are pre-compiled."
+      echo "[x] Please see https://github.com/trailofbits/cxx-common to build dependencies from source."
       return 1
     ;;
   esac
@@ -82,7 +77,8 @@ function GetUbuntuOSVersion
 # Figure out the architecture of the current machine.
 function GetArchVersion
 {
-  local version=$( uname -m )
+  local version
+  version="$( uname -m )"
 
   case "${version}" in
     x86_64)
@@ -93,20 +89,33 @@ function GetArchVersion
       ARCH_VERSION=amd64
       return 0
     ;;
+    aarch64)
+      ARCH_VERSION=aarch64
+      return 0
+    ;;
     *)
-      echo "[x] ${version} architecture is not supported. Only x86_64 (i.e. amd64) are supported."
+      echo "[x] ${version} architecture is not supported. Only aarch64 and x86_64 (i.e. amd64) are supported."
       return 1
     ;;
   esac
 }
 
-function DownloadCxxCommon
+function DownloadVcpkgLibraries
 {
   local GITHUB_LIBS="${LIBRARY_VERSION}.tar.xz"
   local URL="https://github.com/trailofbits/cxx-common/releases/download/${CXX_COMMON_VERSION}/${GITHUB_LIBS}"
 
-  echo "Fetching: ${URL}"
-  if ! curl -LO "${URL}"; then
+  mkdir -p "${DOWNLOAD_DIR}"
+  pushd "${DOWNLOAD_DIR}" || return 1
+
+  if test -e "${GITHUB_LIBS}"
+    then zflag=(-z "${GITHUB_LIBS}")
+    else zflag=()
+  fi
+
+  echo "Fetching: ${URL} and placing in ${DOWNLOAD_DIR}"
+  if ! curl -o "${GITHUB_LIBS}" "${zflag[@]}" -L "${URL}"; then
+    echo "Curl failed"
     return 1
   fi
 
@@ -115,45 +124,18 @@ function DownloadCxxCommon
     TAR_OPTIONS=""
   fi
 
-  tar -xJf "${GITHUB_LIBS}" ${TAR_OPTIONS}
-  rm "${GITHUB_LIBS}"
+  (
+    set -x
+    tar -xJf "${GITHUB_LIBS}" ${TAR_OPTIONS}
+  ) || return $?
+  popd || return 1
 
   # Make sure modification times are not in the future.
-  find "${BUILD_DIR}/libraries" -type f -exec touch {} \;
-
-  if [ -z ${TRAILOFBITS_LIBRARIES+x} ]
-  then
-    # needed for cmake to find our packages
-    export TRAILOFBITS_LIBRARIES="${BUILD_DIR}/libraries"
-  fi
+  find "${DOWNLOAD_DIR}/${LIBRARY_VERSION}" -type f -exec touch {} \;
 
   return 0
 }
 
-function DownloadZ3
-{
-
-  if [[ -d "${LIBRARIES}/z3" ]]
-  then
-    echo "[+] Z3 already downloaded, skipping"
-    return 0
-  fi
-
-  if ! curl -OL "https://github.com/Z3Prover/z3/releases/download/z3-${Z3_VERSION}/${Z3_ARCHIVE}.zip"; then
-    return 1
-  fi
-
-  unzip -qq "${Z3_ARCHIVE}.zip"
-  rm "${Z3_ARCHIVE}.zip"
-  mv "${Z3_ARCHIVE}" "${LIBRARIES}/z3"
-
-  # Make sure modification times are not in the future.
-  find "${LIBRARIES}" -type f -exec touch {} \;
-
-  return 0
-}
-
-# Attempt to detect the OS distribution name.
 # Attempt to detect the OS distribution name.
 function GetOSVersion
 {
@@ -165,18 +147,13 @@ function GetOSVersion
       return 0
     ;;
 
-    *opensuse*)
-      OS_VERSION=opensuse
-      return 0
-    ;;
-
     *arch*)
-      OS_VERSION=ubuntu16.04
+      OS_VERSION=ubuntu-18.04
       return 0
     ;;
 
     [Kk]ali)
-      OS_VERSION=ubuntu18.04
+      OS_VERSION=ubuntu-18.04
       return 0;
     ;;
 
@@ -187,70 +164,36 @@ function GetOSVersion
   esac
 }
 
-# Attempt to determine Z3 version from ARCH_VERSION and OS_VERSION
-function GetZ3ArchiveName
-{
-  if [[ "$ARCH_VERSION" == "amd64" ]]; then
-    Z3_ARCH_VERSION="x64"
-  else
-    echo "[x] Z3 does not support ${ARCH_VERSION}."
-    return 1
-  fi
-
-  case "${OS_VERSION}" in
-    ubuntu14.04)
-      Z3_OS_VERSION="ubuntu-14.04"
-    ;;
-
-    ubuntu16.04)
-      Z3_OS_VERSION="ubuntu-16.04"
-    ;;
-
-    ubuntu18.04)
-      Z3_OS_VERSION="ubuntu-16.04"
-    ;;
-
-    ubuntu19.10)
-      Z3_OS_VERSION="ubuntu-16.04"
-    ;;
-
-    ubuntu20.04)
-      Z3_OS_VERSION="ubuntu-16.04"
-    ;;
-
-    osx)
-      Z3_OS_VERSION="osx"
-    ;;
-
-    *)
-      echo "[x] Z3 does not support ${OS_VERSION}."
-      return 1
-    ;;
-  esac
-
-  Z3_ARCHIVE="z3-$Z3_VERSION-$Z3_ARCH_VERSION-$Z3_OS_VERSION"
-}
-
 # Download pre-compiled version of cxx-common for this OS. This has things like
-# google protobuf, gflags, glog, gtest, capstone, and llvm in it. Also download
-# prebuilt z3 from https://github.com/Z3Prover/z3/releases/.
+# google protobuf, gflags, glog, gtest, capstone, and llvm in it.
 function DownloadLibraries
 {
   # macOS packages
   if [[ "${OSTYPE}" = "darwin"* ]]; then
 
     # Compute an isysroot from the SDK root dir.
-    local sdk_root="${SDKROOT}"
-    if [[ "x${sdk_root}x" = "xx" ]]; then
-      sdk_root=$(xcrun -sdk macosx --show-sdk-path)
-    fi
+    #local sdk_root="${SDKROOT}"
+    #if [[ "x${sdk_root}x" = "xx" ]]; then
+    #  sdk_root=$(xcrun -sdk macosx --show-sdk-path)
+    #fi
 
-    BUILD_FLAGS="${BUILD_FLAGS} -DCMAKE_OSX_SYSROOT=${sdk_root}"
-    OS_VERSION=macos
+    #BUILD_FLAGS="${BUILD_FLAGS} -DCMAKE_OSX_SYSROOT=${sdk_root}"
+    # Min version supported
+    OS_VERSION="macos-10.15"
+    XCODE_VERSION="12.1.0"
     if [[ "$(sw_vers -productVersion)" == "10.15"* ]]; then
       echo "Found MacOS Catalina"
+      OS_VERSION="macos-10.15"
+      # Hard-coded to match pre-built binaries in CI
+      XCODE_VERSION="12.1.0"
+    elif [[ "$(sw_vers -productVersion)" == "11."* ]]; then
+      echo "Found MacOS Big Sur"
+      OS_VERSION="macos-11.0"
+      # Hard-coded to match pre-built binaries in CI
+      XCODE_VERSION="12.2.0"
     else
       echo "WARNING: ****Likely unsupported MacOS Version****"
+      echo "WARNING: ****Using ${OS_VERSION}****"
     fi
 
   # Linux packages
@@ -267,27 +210,19 @@ function DownloadLibraries
     return 1
   fi
 
-  if [[ "${OS_VERSION}" == "macos" ]]; then
-    # Only support catalina build, for now
-    LIBRARY_VERSION="libraries-catalina-macos"
+  if [[ "${OS_VERSION}" == "macos-"* ]]; then
+    # TODO Figure out Xcode compatibility
+    LIBRARY_VERSION="vcpkg_${OS_VERSION}_${LLVM_VERSION}_xcode-${XCODE_VERSION}_${ARCH_VERSION}"
   else
-    LIBRARY_VERSION="libraries-${LLVM_VERSION}-${OS_VERSION}-${ARCH_VERSION}"
-  fi
-
-  if ! GetZ3ArchiveName; then
-      return 1
+    # TODO Arch version
+    LIBRARY_VERSION="vcpkg_${OS_VERSION}_${LLVM_VERSION}_${ARCH_VERSION}"
   fi
 
   echo "[-] Library version is ${LIBRARY_VERSION}"
-  echo "[-] Z3 version is ${Z3_ARCHIVE}"
 
-  if [[ ! -d "${BUILD_DIR}/libraries" ]]; then
-    if ! DownloadCxxCommon; then
-      echo "[x] Unable to download cxx-common build ${LIBRARY_VERSION}."
-      return 1
-    fi
-    if ! DownloadZ3; then
-      echo "[x] Unable to download z3 build ${Z3_ARCHIVE}."
+  if [[ ! -d "${DOWNLOAD_DIR}/${LIBRARY_VERSION}" ]]; then
+    if ! DownloadVcpkgLibraries; then
+      echo "[x] Unable to download vcpkg libraries build ${LIBRARY_VERSION}."
       return 1
     fi
   fi
@@ -298,34 +233,17 @@ function DownloadLibraries
 # Configure the build.
 function Configure
 {
-  # Tell the rellic CMakeLists.txt where the extracted libraries are.
-  echo "[+] Configuring..."
-  export PATH="${LIBRARIES}/cmake/bin:${LIBRARIES}/llvm/bin:${PATH}"
-
-  if [[ "${USE_HOST_COMPILER}" = "1" ]] ; then
-    if [[ "x${CC}x" = "xx" ]] ; then
-      export CC=$(which cc)
-    fi
-
-    if [[ "x${CXX}x" = "xx" ]] ; then
-      export CXX=$(which c++)
-    fi
-  else
-    export CC="${LIBRARIES}/llvm/bin/clang"
-    export CXX="${LIBRARIES}/llvm/bin/clang++"
-  fi
-
-  # Configure the rellic build, specifying that it should use the pre-built
+  # Configure the remill build, specifying that it should use the pre-built
   # Clang compiler binaries.
-  "${LIBRARIES}/cmake/bin/cmake" \
-      "-DZ3_INSTALL_PREFIX=${LIBRARIES}/z3" \
-      -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
-      -DCMAKE_C_COMPILER="${CC}" \
-      -DCMAKE_CXX_COMPILER="${CXX}" \
-      -DCMAKE_VERBOSE_MAKEFILE=True \
-      ${BUILD_FLAGS} \
-      -G Ninja \
-      "${SRC_DIR}"
+  (
+    set -x
+    cmake \
+        -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+        -DCMAKE_VERBOSE_MAKEFILE=True \
+        -DVCPKG_ROOT="${DOWNLOAD_DIR}/${LIBRARY_VERSION}" \
+        ${BUILD_FLAGS} \
+        "${SRC_DIR}"
+  ) || exit $?
 
   return $?
 }
@@ -333,8 +251,52 @@ function Configure
 # Compile the code.
 function Build
 {
-  echo "[+] Building..."
-  "${LIBRARIES}/cmake/bin/cmake" --build .
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    NPROC=$( sysctl -n hw.logicalcpu )
+  else
+    NPROC=$( nproc )
+  fi
+
+  (
+    set -x
+    cmake --build . -- -j"${NPROC}"
+  ) || return $?
+
+  return $?
+}
+
+# Create the packages
+function Package
+{
+  tag_count=$(cd "${SRC_DIR}" && git tag | wc -l)
+  if [[ ${tag_count} == 0 ]]; then
+    echo "WARNING: No tag found, marking this release as 0.0.0"
+    rellic_tag="v0.0.0"
+  else
+    rellic_tag=$(cd "${SRC_DIR}" && git describe --tags --always --abbrev=0)
+  fi
+
+  rellic_commit=$(cd "${SRC_DIR}" && git rev-parse HEAD | cut -c1-7)
+  rellic_version="${rellic_tag:1}.${rellic_commit}"
+
+  (
+    set -x
+
+    if [[ -d "install" ]]; then
+      rm -rf "install"
+    fi
+
+    mkdir "install"
+    export DESTDIR="$(pwd)/install"
+
+    cmake --build . \
+      --target install
+
+    cpack -D RELLIC_DATA_PATH="${DESTDIR}" \
+      -R ${rellic_version} \
+      --config "${SRC_DIR}/packaging/main.cmake"
+  ) || return $?
+
   return $?
 }
 
@@ -343,82 +305,56 @@ function Build
 function GetLLVMVersion
 {
   case ${1} in
-    3.5)
-      LLVM_VERSION=llvm350
-      USE_HOST_COMPILER=1
+    9)
+      LLVM_VERSION=llvm-9
       return 0
     ;;
-    3.6)
-      LLVM_VERSION=llvm360
-      USE_HOST_COMPILER=1
+    10)
+      LLVM_VERSION=llvm-10
       return 0
     ;;
-    3.7)
-      LLVM_VERSION=llvm370
-      USE_HOST_COMPILER=1
+    11)
+      LLVM_VERSION=llvm-11
       return 0
     ;;
-    3.8)
-      LLVM_VERSION=llvm380
-      USE_HOST_COMPILER=1
-      return 0
-    ;;
-    3.9)
-      LLVM_VERSION=llvm390
-      USE_HOST_COMPILER=1
-      return 0
-    ;;
-    4.0)
-      LLVM_VERSION=llvm401
-      USE_HOST_COMPILER=1
-      return 0
-    ;;
-    5.0)
-      LLVM_VERSION=llvm500
-      return 0
-    ;;
-    6.0)
-      LLVM_VERSION=llvm600
-      return 0
-    ;;
-    7.0)
-      LLVM_VERSION=llvm700
-      return 0
-    ;;
-    8.0)
-      LLVM_VERSION=llvm800
-      return 0
-    ;;
-    9.0)
-      LLVM_VERSION=llvm900
-      return 0
-    ;;
-    10.0)
-      LLVM_VERSION=llvm1000
-      return 0
-    ;;
-    llvm*)
-      # assume user specified an exact revision manually
-      LLVM_VERSION=${1}
-      return 0
-    ;;
-
     *)
       # unknown option
-      echo "[x] Unknown LLVM version ${1}."
+      echo "[x] Unknown LLVM version ${1}. You may be able to manually build it with cxx-common."
+      return 1
     ;;
   esac
   return 1
 }
 
+function Help
+{
+  echo "Beginner build script to get started"
+  echo ""
+  echo "Options:"
+  echo "  --prefix           Change the default (${INSTALL_DIR}) installation prefix."
+  echo "  --llvm-version     Change the default (9) LLVM version."
+  echo "  --build-dir        Change the default (${BUILD_DIR}) build directory."
+  echo "  --debug            Build with Debug symbols."
+  echo "  --extra-cmake-args Extra CMake arguments to build with."
+  echo "  -h --help          Print help."
+}
+
 function main
 {
-  local build_only=False
-
   while [[ $# -gt 0 ]] ; do
     key="$1"
 
     case $key in
+
+      -h)
+        Help
+        exit 0
+      ;;
+
+      --help)
+        Help
+        exit 0
+      ;;
 
       # Change the default installation prefix.
       --prefix)
@@ -443,6 +379,13 @@ function main
         shift # past argument
       ;;
 
+      # Change the default download directory.
+      --download-dir)
+        DOWNLOAD_DIR=$(python3 -c "import os; import sys; sys.stdout.write(os.path.abspath('${2}'))")
+        echo "[+] New download directory is ${BUILD_DIR}"
+        shift # past argument
+      ;;
+
       # Make the build type to be a debug build.
       --debug)
         BUILD_FLAGS="${BUILD_FLAGS} -DCMAKE_BUILD_TYPE=Debug"
@@ -453,22 +396,6 @@ function main
         BUILD_FLAGS="${BUILD_FLAGS} ${2}"
         echo "[+] Will supply additional arguments to cmake: ${BUILD_FLAGS}"
         shift
-      ;;
-
-      --use-host-compiler)
-        USE_HOST_COMPILER=1
-        echo "[+] Forcing use of host compiler for build"
-      ;;
-
-      --build-only)
-        build_only=True
-        if [ -z ${TRAILOFBITS_LIBRARIES+x} ]
-        then
-          export LIBRARIES=/opt/trailofbits/libraries
-        else
-          export LIBRARIES=${TRAILOFBITS_LIBRARIES}
-        fi
-        echo "[+] Assuming pre-made libraries exist in ${LIBRARIES}"
       ;;
 
       *)
@@ -484,21 +411,9 @@ function main
   mkdir -p "${BUILD_DIR}"
   cd "${BUILD_DIR}" || exit 1
 
-  local build_it=False
-  if [[ "${build_only}" = "True" ]]
-  then
-    if (Configure && Build); then
-      build_it=True
-    fi
-  else
-    if (DownloadLibraries && Configure && Build); then
-      build_it=True
-    fi
-  fi
-
-  if [[ "${build_it}" = "False" ]]; then
+  if ! (DownloadLibraries && Configure && Build && Package); then
     echo "[x] Build aborted."
-    return 1
+    exit 1
   fi
 
   return $?

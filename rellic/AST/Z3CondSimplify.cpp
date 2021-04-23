9acc3550932b653e5faa85a0c744107e7819c218
Marek Surovič <marek.surovic@gmail.com>
2021-04-23 18:52:30 +0200

    [WIP] Disables expansion of sign_ext to concat

--- rellic/AST/Z3CondSimplify.cpp
+++ rellic/AST/Z3CondSimplify.cpp
@@ -29,7 +29,9 @@ clang::Expr *Z3CondSimplify::SimplifyCExpr(clang::Expr *c_expr) {
   z3::goal goal(*z3_ctx);
   goal.add(z3_expr);
   // Apply on `z3_simplifier` on condition
-  auto app = z3_simplifier(goal);
+  z3::params p(*z3_ctx);
+  p.set("elim_sign_ext", false);
+  auto app = z3::with(z3_simplifier, p)(goal);
   CHECK(app.size() == 1) << "Unexpected multiple goals in application!";
   auto z3_result = app[0].as_expr();
   return z3_gen->GetOrCreateCExpr(z3_result);

e263db13aec22dba6b9e0c34937bd9f6003969b6
Marek Surovič <marek.surovic@gmail.com>
2021-08-20 18:10:24 +0200

    Marks functions as variadic only if they have at least one named arg (#170)

--- lib/AST/IRToASTVisitor.cpp
+++ lib/AST/IRToASTVisitor.cpp
@@ -63,7 +63,7 @@ clang::QualType IRToASTVisitor::GetQualType(llvm::Type *type) {
         params.push_back(GetQualType(param));
       }
       auto epi = clang::FunctionProtoType::ExtProtoInfo();
-      epi.Variadic = func->isVarArg();
+      epi.Variadic = params.size() > 0U && func->isVarArg();
       result = ast_ctx.getFunctionType(ret, params, epi);
     } break;
 

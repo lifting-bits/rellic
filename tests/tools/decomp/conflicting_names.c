c287a9c7aee61b513eebb8116401e22014a71f9b
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2021-10-07 15:23:44 +0200

    Add conflicting names test Add test where multiple local variables (originally declared in various scope levels) have the same name associated via debug info

---
+++ tests/tools/decomp/conflicting_names.c
@@ -0,0 +1,16 @@
+extern int atoi(const char *);
+
+int main(int argc, char *argv[]) {
+  int ret = 0;
+  if (1 < argc) {
+    int argc = atoi(argv[1]);
+    if (argc > 10) {
+      int argc = 99;
+      ret = argc;
+    }
+  } else {
+    int argv = 1;
+    ret = argv;
+  }
+  return ret;
+}

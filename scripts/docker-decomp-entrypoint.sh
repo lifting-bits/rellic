bfeb572b1537fba1d3511f572ad115d2e1b4e3c5
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2021-12-23 12:07:22 +0100

    Fix CI

--- scripts/docker-decomp-entrypoint.sh
+++ scripts/docker-decomp-entrypoint.sh
@@ -2,15 +2,4 @@
 
 # Needed to process multiple arguments to docker image
 
-V=""
-case ${LLVM_VERSION} in
-  llvm12*)
-    V=12.0
-  ;;
-  *)
-    echo "Unknown LLVM version: ${LLVM_VERSION}"
-    exit 1
-  ;;
-esac
-
-/opt/trailofbits/bin/rellic-decomp-${V} "$@"
+/opt/trailofbits/bin/rellic-decomp "$@"

134c53ab80378911069f23608bff955a22b7a5ee
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2022-01-21 12:40:02 +0100

    Update copyright info

--- tools/xref/cgi-bin/decompile.py
+++ tools/xref/cgi-bin/decompile.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+#
+# Copyright (c) 2022-present, Trail of Bits, Inc.
+# All rights reserved.
+#
+# This source code is licensed in accordance with the terms specified in
+# the LICENSE file found in the root directory of this source tree.
+#
+
 import cgi
 import cgitb
 import sys

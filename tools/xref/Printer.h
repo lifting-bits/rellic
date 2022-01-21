b476fff12acc8cb09fa417bdba32c85410c2e0a5
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2022-01-21 12:42:25 +0100

    Add copyright

--- tools/xref/Printer.h
+++ tools/xref/Printer.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2022-present, Trail of Bits, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed in accordance with the terms specified in
+ * the LICENSE file found in the root directory of this source tree.
+ */
+
 #pragma once
 
 #include <clang/AST/ASTContext.h>

30f1b2cc690181b1306a0ed690e32432272dafd6
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2022-04-14 18:52:31 +0200

    Use `printf` instead of `puts`

--- tests/tools/decomp/goto_loop.c
+++ tests/tools/decomp/goto_loop.c
@@ -1,16 +1,16 @@
-int puts(const char*);
+int printf(const char*, ...);
 
 int main() {
   int i = 0;
   start:
   i++;
   if(i == 1) {
-    puts("1\n");
+    printf("1\n");
     goto start;
   } else if(i == 2) {
-    puts("2\n");
+    printf("2\n");
     goto start;
   } else if(i == 3) {
-    puts("3\n");;
+    printf("3\n");;
   }
 }
--- tests/tools/decomp/switch_loop.c
+++ tests/tools/decomp/switch_loop.c
@@ -1,12 +1,12 @@
-int puts(const char*);
+int printf(const char*, ...);
 
 int main() {
   int i = 0;
   start:
   i++;
   switch(i) {
-    case 1: puts("1\n"); goto start; break;
-    case 2: puts("2\n"); goto start; break;
-    case 3: puts("3\n"); break;
+    case 1: printf("1\n"); goto start; break;
+    case 2: printf("2\n"); goto start; break;
+    case 3: printf("3\n"); break;
   }
 }

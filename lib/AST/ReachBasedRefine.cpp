e42e510b805dee4d7ca90b3c7d716f3e3bb02ed5
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2022-08-12 10:49:51 +0200

    Refactor RBR

--- lib/AST/ReachBasedRefine.cpp
+++ lib/AST/ReachBasedRefine.cpp
@@ -11,6 +11,8 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
+#include "rellic/AST/Util.h"
+
 namespace rellic {
 
 ReachBasedRefine::ReachBasedRefine(Provenance &provenance, clang::ASTUnit &unit)
@@ -30,37 +32,103 @@ bool ReachBasedRefine::VisitCompoundStmt(clang::CompoundStmt *compound) {
   z3::expr_vector conds{*z3_ctx};
   bool done_something{false};
   for (size_t i{0}; i < body.size(); ++i) {
-    if (auto if_stmt = clang::dyn_cast<clang::IfStmt>(body[i])) {
-      ifs.push_back(if_stmt);
-      auto cond{GetZ3Cond(if_stmt)};
-      if (!if_stmt->getElse() && Prove(*z3_ctx, !(cond && z3::mk_or(conds)))) {
-        conds.push_back(GetZ3Cond(if_stmt));
+    auto if_stmt{clang::dyn_cast<clang::IfStmt>(body[i])};
+    if (!if_stmt) {
+      ifs.clear();
+      conds.resize(0);
+      continue;
+    }
 
-        if (Prove(*z3_ctx, z3::mk_or(conds)) && ifs.size() > 2) {
-          auto last_if{ifs[0]};
-          for (auto stmt : ifs) {
-            if (stmt == ifs.front()) {
-              continue;
-            }
-            if (stmt == ifs.back()) {
-              last_if->setElse(stmt->getThen());
-            } else {
-              last_if->setElse(stmt);
-              last_if = stmt;
-            }
-          }
+    ifs.push_back(if_stmt);
+    auto cond{GetZ3Cond(if_stmt)};
 
-          size_t start_delete{i - (ifs.size() - 2)};
-          size_t end_delete{i};
-          body.erase(body.erase(std::next(body.begin(), start_delete),
-                                std::next(body.begin(), end_delete)));
-          done_something = true;
-          break;
-        }
+    if (if_stmt->getElse()) {
+      // We cannot link `if` statements that contain `else` branches
+      ifs.clear();
+      conds.resize(0);
+      continue;
+    }
+
+    // Is the current `if` statement unreachable from all the others?
+    bool is_unreachable{Prove(*z3_ctx, !(cond && z3::mk_or(conds)))};
+
+    if (!is_unreachable) {
+      ifs.clear();
+      conds.resize(0);
+      continue;
+    }
+
+    conds.push_back(GetZ3Cond(if_stmt));
+
+    // Do the collected statements cover all possibilities?
+    auto is_complete{Prove(*z3_ctx, z3::mk_or(conds))};
+
+    if (ifs.size() <= 2 || !is_complete) {
+      // We need to collect more statements
+      continue;
+    }
+
+    /*
+    `body` will look like this at this point:
+
+      ...
+      i - n    : ...
+      i - n + 1: if(cond_1) { }
+      i - n + 2: if(cond_2) { }
+      ...
+      i - 1    : if(cond_n-1) { }
+      i        : if(cond_n) { }
+      ...
+
+    and we want to chain all of the statements together:
+      ...
+      i - n    : ...
+      i - n + 1: if(cond_1) { } else if(cond_2) { } ... else if(cond_n) { }
+      i - n + 2: if(cond_2) { } else if(cond_3) { } ... else if(cond_n) { }
+      ...
+      i - 1    : if(cond_n-1) { } else if(cond_n) { }
+      i        : if(cond_n) { }
+      ...
+    */
+    auto last_if{ifs[0]};
+    for (auto stmt : ifs) {
+      if (stmt == ifs.front()) {
+        continue;
+      }
+      if (stmt == ifs.back()) {
+        last_if->setElse(stmt->getThen());
+      } else {
+        last_if->setElse(stmt);
+        last_if = stmt;
       }
     }
-    ifs.clear();
-    conds.resize(0);
+
+    /*
+    `body` will look like this at this point:
+
+      ...
+      i - n    : ...
+      i - n + 1: if(cond_1) { } else if(cond_2) { } ... else if(cond_n) { }
+      i - n + 2: if(cond_2) { } else if(cond_3) { } ... else if(cond_n) { }
+      ...
+      i - 1    : if(cond_n-1) { } else if(cond_n) { }
+      i        : if(cond_n) { }
+      ...
+
+    but since we chained all of the statements into the first, we want to remove
+    the others from the body:
+
+      ...
+      i - n    : ...
+      i - n + 1: if(cond_1) { } else if(cond_2) { } else if ...
+      ...
+    */
+    size_t start_delete{i - (ifs.size() - 2)};
+    size_t end_delete{i};
+    body.erase(body.erase(std::next(body.begin(), start_delete),
+                          std::next(body.begin(), end_delete)));
+    done_something = true;
+    break;
   }
 
   if (done_something) {
